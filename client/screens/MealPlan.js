0d6ad49a69fe6304c416c884f992c4c7a9342b87
btsang <105647814+btsangk20@users.noreply.github.com>
2023-12-04 21:48:19 +0700

    feat: add tailwind css & tab navigation (#2)

---
+++ client/screens/MealPlan.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const MealPlan = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>MealPlan</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default MealPlan;
+
---
+++ client/screens/Profile.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Profile</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
+
