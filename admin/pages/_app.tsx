d6c939074feb4e96b8f09949aff2722061a36870
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-01-20 10:29:45 +0700

    Change to refine

---
+++ admin/pages/_app.tsx
@@ -0,0 +1,113 @@
+import {
+  ThemedLayoutV2,
+  ThemedSiderV2,
+  useNotificationProvider,
+} from "@refinedev/antd";
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+import type { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import { Header } from "@components/header";
+import { ColorModeContextProvider } from "@contexts";
+import "@refinedev/antd/dist/reset.css";
+import { DataProvider } from "@refinedev/strapi-v4";
+import { App as AntdApp } from "antd";
+import { appWithTranslation, useTranslation } from "next-i18next";
+import { authProvider, axiosInstance } from "src/authProvider";
+import { API_URL } from "src/constants";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return (
+      <ThemedLayoutV2
+        Header={() => <Header sticky />}
+        Sider={(props) => <ThemedSiderV2 {...props} fixed />}
+      >
+        <Component {...pageProps} />
+      </ThemedLayoutV2>
+    );
+  };
+
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                routerProvider={routerProvider}
+                authProvider={authProvider}
+                dataProvider={DataProvider(API_URL + `/api`, axiosInstance)}
+                notificationProvider={useNotificationProvider}
+                i18nProvider={i18nProvider}
+                resources={[
+                  {
+                    name: "blog-posts",
+                    list: "/blog-posts",
+                    create: "/blog-posts/create",
+                    edit: "/blog-posts/edit/:id",
+                    show: "/blog-posts/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                  {
+                    name: "categories",
+                    list: "/categories",
+                    create: "/categories/create",
+                    edit: "/categories/edit/:id",
+                    show: "/categories/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                ]}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "XrrjTR-aBdneH-wM7Log",
+                }}
+              >
+                {renderComponent()}
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default appWithTranslation(MyApp);
---
+++ admin/pages/blog-posts/edit/[id].tsx
@@ -0,0 +1,44 @@
+import { AntdEditInferencer } from "@refinedev/inferencer/antd";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+export default function BlogPostEdit() {
+  return (
+    <AntdEditInferencer
+      fieldTransformer={(field) => {
+        if (["locale", "updatedAt", "publishedAt"].includes(field.key)) {
+          return false;
+        }
+
+        return field;
+      }}
+    />
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
