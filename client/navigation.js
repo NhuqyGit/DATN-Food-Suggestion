613d088b6e389b50e21c8ef38b00c554426c139b
Tan Sang <btsang20@clc.fitus.edu.vn>
2024-01-21 21:46:33 +0700

    refactor: revert code

--- client/navigation.js
+++ client/navigation.js
@@ -1,38 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import React from 'react';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import Profile from './screens/Profile';
 import MealPlan from './screens/MealPlan';
 import Personalization from './screens/Personalization';
-import FoodDetailsScreen from './components/FoodDetails/FoodDetails.jsx';
-
-// const Stack = createNativeStackNavigator();
+import FoodDetailsScreen from './screens/FoodDetails';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Profile' component={Profile} />
-        <Stack.Screen name='MealPlan' component={MealPlan} />
-        <Stack.Screen name='Personalization' component={Personalization} />
-      </Stack.Navigator> */}
-
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
         }}
       >
-        <Tab.Screen name='Home' component={FoodDetailsScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} />
         <Tab.Screen name='Profile' component={Profile} />
         <Tab.Screen name='MealPlan' component={MealPlan} />
         <Tab.Screen name='Personalization' component={Personalization} />
