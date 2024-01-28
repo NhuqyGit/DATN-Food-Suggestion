07bd831ea29509b00ef587ad812906d362bf5bc9
btsang <105647814+btsangk20@users.noreply.github.com>
2024-01-28 10:17:12 +0700

    feat: food details (#10)
    * Food details screen(still in progress...)
    
    * fix dump
    
    * refactor: format source
    
    * refactor: revert code
    
    * UI: details dish
    
    * UI: add report model, save model, review model
    
    ---------
    
    Co-authored-by: unknown <phanthihuunien@gmail.com>

--- client/navigation.js
+++ client/navigation.js
@@ -1,32 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import React from 'react';
-import HomeScreen from './screens/HomeScreen';
-import Profile from './screens/Profile';
-import MealPlan from './screens/MealPlan';
-import Personalization from './screens/Personalization';
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import Profile from "./screens/Profile";
+import MealPlan from "./screens/MealPlan";
+import Personalization from "./screens/Personalization";
+import FoodDetailsScreen from "./components/FoodDetails/FoodDetails";
 import Icon from 'react-native-vector-icons/FontAwesome';
-
-// const Stack = createNativeStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Profile' component={Profile} />
-        <Stack.Screen name='MealPlan' component={MealPlan} />
-        <Stack.Screen name='Personalization' component={Personalization} />
-      </Stack.Navigator> */}
-
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
@@ -82,4 +69,3 @@ export default function Navigation() {
     </NavigationContainer>
   );
 }
-
