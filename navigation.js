3b5cb482c5df3e4d994d6d452903590f3f936c7f
NhuqyGit <kathonflag@gmail.com>
2024-04-05 09:14:46 +0700

    [IMP][FoodSuggestion] Add drawer navigation

--- navigation.js
+++ navigation.js
@@ -54,7 +54,7 @@ const TabArr = [
 ]
 
 export default function Navigation() {
-  const [isDone, setIsDone] = useState(true)
+  const [isDone, setIsDone] = useState(false)
 
   return (
     <NavigationContainer>
