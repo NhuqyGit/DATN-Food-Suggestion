462fcaa94939af59a3906b0bdf350c34234432bc
phanthihuunien <81319245+phanthihuunien@users.noreply.github.com>
2024-04-07 16:06:52 +0700

    feat: explore categories screen

--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import HomeScreen from './HomeScreen'
 import FoodDetailsScreen from '../../components/FoodDetails/FoodDetails'
 import CollectionScreen from '../../components/FoodDetails/CollectionScreen'
-
+import ExploreCategories from '../../components/CategoryList/ExploreCategories'
+import AddNewCollection from '../../components/FoodDetails/AddNewCollection'
 const Stack = createNativeStackNavigator()
 function Home() {
   return (
@@ -13,8 +14,10 @@ function Home() {
       }}
     >
       <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      <Stack.Screen name='ExploreCategories' component={ExploreCategories} />
       <Stack.Screen name='FoodDetail' component={FoodDetailsScreen} />
       <Stack.Screen name="CollectionScreen" component={CollectionScreen} />
+      <Stack.Screen name="AddNewCollection" component={AddNewCollection} />
     </Stack.Navigator>
   )
 }
