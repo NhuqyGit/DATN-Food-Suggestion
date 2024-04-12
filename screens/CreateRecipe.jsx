6a4bf565bb8dcda8eb0a5c31d18fd69c400d8bc2
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2024-04-12 19:25:24 +0700

    feat : first tab

--- screens/CreateRecipe.jsx
+++ screens/CreateRecipe.jsx
@@ -8,15 +8,132 @@ import {
   Image,
   Dimensions,
   ImageBackground,
+  Modal,
+  Pressable,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import { theme } from "../theme/index";
 import { TabView, SceneMap } from "react-native-tab-view";
+import { FlatList } from "react-native-gesture-handler";
+import * as ImagePicker from "expo-image-picker";
 
-const FirstTab = ({ igredientList, createIngredient, deleteIngredient }) => (
-  <View style={{ flex: 1, backgroundColor: "#ff4081" }} />
-);
+const FirstTab = ({
+  ingredientList,
+  createIngredient,
+  deleteIngredient,
+  updateIngredient,
+}) => {
+  const [modalVisible, setModalVisible] = useState("false");
+  const [modalType, setModalType] = useState("");
+  const [ingredientInput, setIngradientInput] = useState("");
+  const [ingredientID, setIngredientID] = useState(0);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#ff4081" }}>
+      <FlatList
+        data={ingredientList}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                setIngredientID(item.id);
+                setModalType("update");
+                setModalVisible(true);
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text>{item.name}</Text>
+                <TouchableOpacity
+                  onPress={() => {
+                    deleteIngredient(item.id);
+                  }}
+                >
+                  <Text> button</Text>
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={createVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+        style
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              {modalType == "create"
+                ? "New ingredient"
+                : modalType == "update"
+                  ? "Update ingredient"
+                  : "Undefined Type"}
+            </Text>
+            <TextInput
+              style={styles.input}
+              value={ingredientInput}
+              onChangeText={setIngradientInput}
+            />
+            <View
+              style={{
+                flexDirection: "row-reverse",
+                justifyContent: "center",
+                alignContent: "center",
+              }}
+            >
+              <Pressable
+                style={[styles.button]}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text
+                  style={[styles.textStyle, { color: theme.colors.secondary }]}
+                >
+                  Cancel
+                </Text>
+              </Pressable>
+              <Pressable
+                style={[
+                  styles.button,
+                  { backgroundColor: theme.colors.secondary },
+                ]}
+                onPress={() => {
+                  if (modalType == "create") {
+                    createIngredient(ingredientInput);
+                  } else if (modalType == "update") {
+                    updateIngredient(ingredientID, ingredientInput);
+                  }
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>
+                  {modalType == "create"
+                    ? "Create"
+                    : modalType == "update"
+                      ? "Update"
+                      : "Undefined"}
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
 
 const SecondTab = () => (
   <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
@@ -32,9 +149,26 @@ function CreateRecipe() {
     { key: "second", title: "Instructions" },
     { key: "third", title: "Presentation" },
   ]);
-
-  function deleteIngredient(index, ingredient) {}
-  function createIngredient(ingredient) {}
+  const [ingredientList, setIngradientList] = useState([]);
+  function deleteIngredient(id) {
+    setIngradientList(ingredientList.filter((e) => e.id == id));
+  }
+  function createIngredient(ingredientName) {
+    setIngradientList([
+      ...ingredientList,
+      { id: ingredientList.slice(-1).id + 1, name: ingredientName },
+    ]);
+  }
+  function updateIngredient(id, ingredientName) {
+    setIngradientList(
+      ingredientList.map((ingredient) => {
+        if (ingredient.id == id) {
+          ingredient.name = ingredientName;
+        }
+        return ingredient;
+      })
+    );
+  }
 
   const renderScene = ({ route, jumpTo }) => {
     switch (route.key) {
@@ -42,8 +176,10 @@ function CreateRecipe() {
         return (
           <FirstTab
             jumpTo={jumpTo}
+            ingredientList={ingredientList}
             deleteIngredient={deleteIngredient}
             createIngredient={createIngredient}
+            updateIngredient={updateIngredient}
           />
         );
       case "second":
@@ -58,7 +194,10 @@ function CreateRecipe() {
       {...props}
       renderLabel={({ route, focused, color }) => (
         <Text
-          style={{ color: focused ? theme.colors.primary : "white", fontSize: 16 }}
+          style={{
+            color: focused ? theme.colors.primary : "white",
+            fontSize: 16,
+          }}
         >
           {route.title}
         </Text>
@@ -77,8 +216,10 @@ function CreateRecipe() {
         onIndexChange={setActiveTab}
         initialLayout={{ width: WIDTH }}
       />
-      <TouchableOpacity style={{position:"absolute", bottom: 20, right: 20}}>
-        <View style={{backgroundColor: 'red', padding: 10, borderRadius: 45}}></View>
+      <TouchableOpacity style={{ position: "absolute", bottom: 20, right: 20 }}>
+        <View
+          style={{ backgroundColor: "red", padding: 10, borderRadius: 45 }}
+        ></View>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -109,6 +250,42 @@ const styles = StyleSheet.create({
     flex: 4,
     alignItems: "center",
     backgroundColor: "white",
+  },
+  input: { height: 40, margin: 12, borderWidth: 1, padding: 10 },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
 
