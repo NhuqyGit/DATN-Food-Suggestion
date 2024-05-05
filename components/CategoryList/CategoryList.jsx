95931ba4c336c71ac6478739db305b28b378f981
Tan Sang <btsang20@clc.fitus.edu.vn>
2024-05-05 13:23:54 +0700

    feat: integrate categories

--- components/CategoryList/CategoryList.jsx
+++ components/CategoryList/CategoryList.jsx
@@ -1,30 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView, StyleSheet, View, Text } from 'react-native'
 import Category from '../Category/Category'
 
 function CategoryList() {
-  const mockData = [
-    {
-      id: 1,
-      title: 'Breakfast',
-      image: require('../../assets/images/Home/category1.png')
-    },
-    {
-      id: 2,
-      title: 'Vegetarian',
-      image: require('../../assets/images/Home/category2.png')
-    },
-    {
-      id: 3,
-      title: 'Beverage',
-      image: require('../../assets/images/Home/category3.png')
-    },
-    {
-      id: 4,
-      title: 'Dessert',
-      image: require('../../assets/images/Home/category4.png')
-    },
-  ]
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const response = await fetch(
+          'https://datn-admin-be.onrender.com/categories'
+        )
+        const json = await response.json()
+        setCategories(json)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getCategories()
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -34,7 +29,7 @@ function CategoryList() {
         horizontal // Enable horizontal scrolling
         showsHorizontalScrollIndicator={false} // Hide the horizontal scroll indicator
       >
-        {mockData?.map((item) => (
+        {categories?.map((item) => (
           <Category key={item.id} item={item} />
         ))}
       </ScrollView>
@@ -47,15 +42,15 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     marginTop: 32,
   },
-  
+
   title: {
     paddingLeft: 20,
     fontSize: 28,
     fontWeight: '700',
     // marginBottom: 15
-    color: "#231F20"
+    color: '#231F20',
   },
-  
+
   list: {
     paddingLeft: 15,
     flexDirection: 'row',
@@ -65,3 +60,4 @@ const styles = StyleSheet.create({
 })
 
 export default CategoryList
+
