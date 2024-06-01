b1d1cc6f0061dbdaa64d7b985c264f21e1062b07
Tan Sang <btsang20@clc.fitus.edu.vn>
2024-06-01 16:58:30 +0700

    feat: update search

--- screens/Search/SearchScreen/SearchScreen.jsx
+++ screens/Search/SearchScreen/SearchScreen.jsx
@@ -20,239 +20,6 @@ const SearchScreen = ({ navigation, route }) => {
   const [isVisible, setIsVisible] = useState(false)
   const [step, setStep] = useState(1)
 
-  const mockHistory = [
-    {
-      id: 1,
-      title: 'Thịt bò',
-    },
-    {
-      id: 2,
-      title: 'Xà lách',
-    },
-    {
-      id: 3,
-      title: 'Dưa cải chua',
-    },
-  ]
-
-  const mockSearchResult = [
-    {
-      id: 1,
-      title: 'Bun bo Hue with new broth, best recipe from around the world',
-      author: 'Master Chef :)',
-      image: require('../../../assets/images/Home/recommend1.png'),
-      ingredients: [
-        'Ingredient 1',
-        'Ingredient 2',
-        'Ingredient 3',
-        'Ingredient 4',
-        'Ingredient 5',
-        'Ingredient 6',
-      ],
-      reviews: [
-        {
-          user: 'User1',
-          comment:
-            'Following the recipe instructions was a breeze, and the end result was truly satisfying. The creamy Alfredo sauce was velvety smooth, perfectly coating each strand of pasta!',
-          rating: 1,
-        },
-        {
-          user: 'User2',
-          comment:
-            'I appreciated the simplicity of the ingredients list. It made it so easy to throw together a delicious meal without needing to make a trip to the store.',
-          rating: 4,
-        },
-        {
-          user: 'User1',
-          comment:
-            'The cooking times for the chicken and pasta were spot on – everything came together perfectly.',
-          rating: 5,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4.5 },
-        {
-          user: 'User1',
-          comment:
-            'I would recommend adding a squeeze of lemon juice to the sauce to brighten up the flavors.!',
-          rating: 2,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 3 },
-        { user: 'User1', comment: 'Delicious!', rating: 3.4 },
-        {
-          user: 'User2',
-          comment:
-            'I loved the creamy texture of the Alfredo sauce, but I felt like it needed a bit more seasoning.!',
-          rating: 4,
-        },
-      ],
-      rating: 4,
-      totalTime: 40,
-      servings: 3,
-      calories: 80,
-    },
-    {
-      id: 2,
-      title: 'Pho with new broth, best recipe from around the world',
-      author: 'Master Chef ;)',
-      image: require('../../../assets/images/Home/recommend2.png'),
-      ingredients: [
-        'Ingredient 1',
-        'Ingredient 2',
-        'Ingredient 3',
-        'Ingredient 4',
-        'Ingredient 5',
-        'Ingredient 6',
-      ],
-      reviews: [
-        {
-          user: 'User1',
-          comment:
-            "Impeccable service and mouthwatering flavors make this a must-visit spot. From the moment you step in, you're treated to a culinary journey that delights the senses!",
-          rating: 3,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-        {
-          user: 'User1',
-          comment:
-            'I would recommend adding a squeeze of lemon juice to the sauce to brighten up the flavors.!',
-          rating: 5,
-        },
-        {
-          user: 'User2',
-          comment:
-            'I loved the creamy texture of the Alfredo sauce, but I felt like it needed a bit more seasoning.',
-          rating: 4.5,
-        },
-        { user: 'User1', comment: 'Delicious!', rating: 2 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 3 },
-        {
-          user: 'User1',
-          comment:
-            'I loved the creamy texture of the Alfredo sauce, but I felt like it needed a bit more seasoning.!',
-          rating: 3.4,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-      ],
-      rating: 4,
-      totalTime: 40,
-      servings: 3,
-      calories: 80,
-    },
-    {
-      id: 3,
-      title: 'Bun bo Hue with new broth, best recipe from around the world',
-      author: 'Master Chef :)',
-      image: require('../../../assets/images/Home/recommend3.png'),
-      ingredients: [
-        'Ingredient 1',
-        'Ingredient 2',
-        'Ingredient 3',
-        'Ingredient 4',
-        'Ingredient 5',
-        'Ingredient 6',
-      ],
-      reviews: [
-        { user: 'User1', comment: 'Delicious!', rating: 1 },
-        {
-          user: 'User2',
-          comment:
-            'I would recommend adding a squeeze of lemon juice to the sauce to brighten up the flavors.!',
-          rating: 4,
-        },
-        {
-          user: 'User1',
-          comment:
-            'The cooking times for the chicken and pasta were spot on – everything came together perfectly.',
-          rating: 5,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4.5 },
-        {
-          user: 'User1',
-          comment:
-            'I loved the creamy texture of the Alfredo sauce, but I felt like it needed a bit more seasoning.',
-          rating: 2,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 3 },
-        { user: 'User1', comment: 'Delicious!', rating: 3.4 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-      ],
-      rating: 4,
-      totalTime: 40,
-      servings: 3,
-      calories: 80,
-    },
-    {
-      id: 4,
-      title: 'Delicious Dish',
-      author: 'Master Chef :)',
-      image: require('../../../assets/images/Home/recommend4.png'),
-      ingredients: [
-        'Ingredient 1',
-        'Ingredient 2',
-        'Ingredient 3',
-        'Ingredient 4',
-        'Ingredient 5',
-        'Ingredient 6',
-      ],
-      reviews: [
-        {
-          user: 'User1',
-          comment:
-            'I loved the creamy texture of the Alfredo sauce, but I felt like it needed a bit more seasoning.!',
-          rating: 1,
-        },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-        {
-          user: 'User1',
-          comment:
-            'I would recommend adding a squeeze of lemon juice to the sauce to brighten up the flavors.!',
-          rating: 5,
-        },
-        {
-          user: 'User2',
-          comment:
-            'I loved the creamy texture of the Alfredo sauce, but I felt like it needed a bit more seasoning.',
-          rating: 4.5,
-        },
-        { user: 'User1', comment: 'Delicious!', rating: 2 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 3 },
-        { user: 'User1', comment: 'Delicious!', rating: 3.4 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-      ],
-      rating: 4,
-      totalTime: 40,
-      servings: 3,
-      calories: 80,
-    },
-    {
-      id: 5,
-      title: 'Delicious Dish',
-      author: 'Master Chef :)',
-      image: require('../../../assets/monngon.jpg'),
-      ingredients: [
-        'Ingredient 1',
-        'Ingredient 2',
-        'Ingredient 3',
-        'Ingredient 4',
-        'Ingredient 5',
-        'Ingredient 6',
-      ],
-      reviews: [
-        { user: 'User1', comment: 'Delicious!', rating: 1 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-        { user: 'User1', comment: 'Delicious!', rating: 5 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4.5 },
-        { user: 'User1', comment: 'Delicious!', rating: 2 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 3 },
-        { user: 'User1', comment: 'Delicious!', rating: 3.4 },
-        { user: 'User2', comment: 'Amazing recipe!', rating: 4 },
-      ],
-      rating: 4,
-      totalTime: 40,
-      servings: 3,
-      calories: 80,
-    },
-  ]
-
   const [ingredients, setIngredients] = useState([])
   const [dish, setDish] = useState([])
   const [searchText, setSearchText] = useState('')
@@ -284,7 +51,7 @@ const SearchScreen = ({ navigation, route }) => {
       setLoadingDish(true)
       try {
         const token = await AsyncStorageService.getAccessToken()
-        const response = await fetch(`${HOST}/dish?sort=desc`, {
+        const response = await fetch(`${HOST}/dish/latest?sort=desc&limit=5`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -307,7 +74,7 @@ const SearchScreen = ({ navigation, route }) => {
     const getDishBySearchText = async () => {
       try {
         const token = await AsyncStorageService.getAccessToken()
-        const response = await fetch(`${HOST}/dish/search/${searchText}`, {
+        const response = await fetch(`${HOST}/dish/search?text=${searchText}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -392,7 +159,7 @@ const SearchScreen = ({ navigation, route }) => {
             <View style={styles.titleContainer}>
               <Text
                 style={styles.titleResult}
-              >{`${mockHistory.length}+ RESULT`}</Text>
+              >{`${dishBySearchText?.length} RESULT`}</Text>
               <TouchableOpacity
                 style={styles.filterContainer}
                 onPress={() => {
