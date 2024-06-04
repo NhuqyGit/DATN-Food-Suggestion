c8f2f1a3f07de77183f6f23eeefdf1006d1570e6
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2024-06-04 14:17:07 +0700

    feat: api intergrating

--- screens/NewCollection.jsx
+++ screens/NewCollection.jsx
@@ -1,115 +1,170 @@
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Ionicons } from '@expo/vector-icons'
-import { StatusBar } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { theme } from '../theme/index'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { theme } from "../theme/index";
+import { useSelector } from "react-redux";
+import { selectUserInfo } from "../slices/userLoginSlice";
 
 const NewCollection = () => {
-    const navigation = useNavigation()
+  const userInfo = useSelector(selectUserInfo);
 
-    const dismissKeyboard = () => {
-        Keyboard.dismiss();
-    };
-    return (
-        <SafeAreaView style={{backgroundColor: 'white'}}>
-            <StatusBar backgroundColor="black" barStyle="dark-content" />
-            <TouchableWithoutFeedback onPress={dismissKeyboard}>
-                <View style={styles.container}>
-                    <TouchableOpacity
-                        onPress={()=>navigation.goBack()}
-                        style={styles.btnBack}>
-                        <Ionicons
-                            name='close'
-                            size={22}
-                            color='black'
-                        />
-                    </TouchableOpacity>
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [clicked, setClicked] = useState(false);
 
-                    <Text style={styles.title}>Add Collection</Text>
+  const navigation = useNavigation();
 
-                    <View style={styles.formCollection}>
-                        <View style={styles.input}>
-                            <TextInput placeholder='Name your collection' />
-                        </View>
+  const handleCreate = async () => {
+    const token = await AsyncStorageService.getAccessToken();
+    try {
+      const response = await fetch(`${HOST}/collections`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          userId: userInfo?.id,
+          name,
+          description: desc,
+        }),
+      });
 
-                        <View style={[styles.input, {height: '40%', paddingTop: 10}]}>
-                            <TextInput
-                                style={{height: '100%'}}
-                                editable
-                                multiline
-                                numberOfLines={4}
-                                placeholder='Add a description (optional)'
-                            />
-                        </View>
-                    </View>
+      const responseJson = await response.json();
 
-                    <View style={styles.btnSaveContainer}>
-                        <TouchableOpacity style={styles.btnSave}>
-                            <Text style={styles.btnText}>Save</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </TouchableWithoutFeedback>
-        </SafeAreaView>
-    )
-}
+      if (responseJson.error) {
+        console.log(responseJson.message);
+      } else {
+        navigation.goBack();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setClicked(false);
+  };
 
-export default NewCollection
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+  return (
+    <SafeAreaView style={{ backgroundColor: "white" }}>
+      <StatusBar backgroundColor="black" barStyle="dark-content" />
+      <TouchableWithoutFeedback onPress={dismissKeyboard}>
+        <View style={styles.container}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.btnBack}
+          >
+            <Ionicons name="close" size={22} color="black" />
+          </TouchableOpacity>
+
+          <Text style={styles.title}>Add Collection</Text>
+
+          <View style={styles.formCollection}>
+            <View style={styles.input}>
+              <TextInput
+                placeholder="Name your collection"
+                value={name}
+                onChangeText={setName}
+              />
+            </View>
+
+            <View style={[styles.input, { height: "40%", paddingTop: 10 }]}>
+              <TextInput
+                style={{ height: "100%" }}
+                editable
+                multiline
+                numberOfLines={4}
+                placeholder="Add a description (optional)"
+                value={desc}
+                onChangeText={setDesc}
+              />
+            </View>
+          </View>
+
+          <View style={styles.btnSaveContainer}>
+            <TouchableOpacity
+              style={styles.btnSave}
+              onPress={() => {
+                setClicked(true);
+                handleCreate();
+              }}
+              disabled={clicked}
+            >
+              <Text style={styles.btnText}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  );
+};
+
+export default NewCollection;
 
 const styles = StyleSheet.create({
-    container:{
-        width: '100%',
-        height: '100%',
-        paddingHorizontal: 20,
-        backgroundColor:'white',
-        // position: 'relative'
-    },
-    btnBack:{
-        width: 35,
-        height: 35,
-        alignSelf: 'flex-end',
-        marginTop: 15,
-        marginBottom: 15,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 50,
-        backgroundColor: '#F3F3F3'
-    },
-    title:{
-        fontSize: 24,
-        fontWeight: '700'
-    },
-    input:{
-        borderRadius: 5,
-        marginTop: 25,
-        padding: 15,
-        backgroundColor: '#F3F3F3',
-    },
-    btnSaveContainer:{
-        position: 'absolute',
-        bottom: 30,
-        left: 0,
-        right: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    btnSave:{
-        backgroundColor: theme.colors.secondary,
-        paddingHorizontal: 30,
-        paddingVertical: 15,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 25,
-        width: '30%'
-    },
-    btnText:{
-        color: 'white',
-        fontSize: 16,
-        fontWeight: '500'
-    }
-})
+  container: {
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: 20,
+    backgroundColor: "white",
+    // position: 'relative'
+  },
+  btnBack: {
+    width: 35,
+    height: 35,
+    alignSelf: "flex-end",
+    marginTop: 15,
+    marginBottom: 15,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    backgroundColor: "#F3F3F3",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+  },
+  input: {
+    borderRadius: 5,
+    marginTop: 25,
+    padding: 15,
+    backgroundColor: "#F3F3F3",
+  },
+  btnSaveContainer: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnSave: {
+    backgroundColor: theme.colors.secondary,
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
+    width: "30%",
+  },
+  btnText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,8 +1,9 @@
-import { MaterialIcons } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
-import { StatusBar } from 'expo-status-bar'
-import React, { useState } from 'react'
+import { MaterialIcons } from "@expo/vector-icons";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
 import {
+  Alert,
   Image,
   Modal,
   Platform,
@@ -12,157 +13,163 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
-} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { COLORS, FONTS } from '../theme/theme'
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS, FONTS } from "../theme/theme";
+import * as ImagePicker from "expo-image-picker";
 
-const img = require('../constants/knife-fork.jpg')
+import { AsyncStorageService } from "../utils/AsynStorage";
+import { selectUserInfo, setUserInfo } from "../slices/userLoginSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { HOST, uploadToFirebase } from "../config";
 
-const data = [
-  { id: 0, name: 'List', date: '12/21/54' },
-  { id: 0, name: 'List', date: '12/21/54' },
-  { id: 0, name: 'List', date: '12/21/54' },
-  { id: 0, name: 'List', date: '12/21/54' },
-]
+//const img = require("../constants/knife-fork.jpg");
 
-function SavedRecipeLists() {
-  const navigation = useNavigation()
-  return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          borderRadius: 15,
-          alignItems: 'center',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingVertical: 5,
-        }}
-      >
-        <TouchableOpacity>
-          <View
-            style={{
-              flexDirection: 'row',
-              backgroundColor: 'orange',
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingHorizontal: 10,
-              paddingVertical: 5,
-              borderRadius: 45,
-            }}
-          >
-            <Text style={{ color: 'white', fontSize: 17, paddingBottom: 2 }}>
-              Sort
-            </Text>
-            <MaterialIcons
-              name='sort-by-alpha'
-              size={24}
-              color='white'
-              style={{ marginLeft: 5 }}
-            />
-          </View>
-        </TouchableOpacity>
+// const data = [
+//   { id: 0, name: "List", date: "12/21/54" },
+//   { id: 0, name: "List", date: "12/21/54" },
+//   { id: 0, name: "List", date: "12/21/54" },
+//   { id: 0, name: "List", date: "12/21/54" },
+// ];
 
-        <TouchableOpacity style={{ marginRight: 5 }}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <MaterialIcons size={24} name='add-box' color='orange' />
-            <Text
-              style={{
-                fontSize: 17,
-                color: 'orange',
-                alignItems: 'center',
-              }}
-            >
-              Add
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-      {data.map((item, index) => {
-        return (
-          <TouchableOpacity
-            key={index}
-            style={[
-              {
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                borderRadius: 15,
-                paddingHorizontal: 20,
-                marginTop: 5,
-                paddingVertical: 5,
-                backgroundColor: 'white',
-              },
-              styles.boxShadow,
-            ]}
-            onPress={() => {
-              navigation.navigate('List', { id: item.id, name: item.name })
-            }}
-          >
-            <Text
-              style={{
-                ...FONTS.h3,
-                color: COLORS.primary,
-                alignItems: 'center',
-              }}
-            >
-              {item.name}
-            </Text>
+// function SavedRecipeLists() {
+//   const navigation = useNavigation();
+//   return (
+//     <View style={{ flex: 1 }}>
+//       <View
+//         style={{
+//           borderRadius: 15,
+//           alignItems: "center",
+//           flexDirection: "row",
+//           justifyContent: "space-between",
+//           paddingVertical: 5,
+//         }}
+//       >
+//         <TouchableOpacity>
+//           <View
+//             style={{
+//               flexDirection: "row",
+//               backgroundColor: "orange",
+//               alignItems: "center",
+//               justifyContent: "center",
+//               paddingHorizontal: 10,
+//               paddingVertical: 5,
+//               borderRadius: 45,
+//             }}
+//           >
+//             <Text style={{ color: "white", fontSize: 17, paddingBottom: 2 }}>
+//               Sort
+//             </Text>
+//             <MaterialIcons
+//               name="sort-by-alpha"
+//               size={24}
+//               color="white"
+//               style={{ marginLeft: 5 }}
+//             />
+//           </View>
+//         </TouchableOpacity>
 
-            <Image
-              source={img}
-              style={{
-                width: 50,
-                height: 50,
-                borderRadius: 45,
-                opacity: 0.5,
-              }}
-            />
-          </TouchableOpacity>
-        )
-      })}
-    </View>
-  )
-}
+//         <TouchableOpacity style={{ marginRight: 5 }}>
+//           <View style={{ flexDirection: "row", alignItems: "center" }}>
+//             <MaterialIcons size={24} name="add-box" color="orange" />
+//             <Text
+//               style={{
+//                 fontSize: 17,
+//                 color: "orange",
+//                 alignItems: "center",
+//               }}
+//             >
+//               Add
+//             </Text>
+//           </View>
+//         </TouchableOpacity>
+//       </View>
+//       {data.map((item, index) => {
+//         return (
+//           <TouchableOpacity
+//             key={index}
+//             style={[
+//               {
+//                 flex: 1,
+//                 flexDirection: "row",
+//                 justifyContent: "space-between",
+//                 alignItems: "center",
+//                 borderRadius: 15,
+//                 paddingHorizontal: 20,
+//                 marginTop: 5,
+//                 paddingVertical: 5,
+//                 backgroundColor: "white",
+//               },
+//               styles.boxShadow,
+//             ]}
+//             onPress={() => {
+//               navigation.navigate("List", { id: item.id, name: item.name });
+//             }}
+//           >
+//             <Text
+//               style={{
+//                 ...FONTS.h3,
+//                 color: COLORS.primary,
+//                 alignItems: "center",
+//               }}
+//             >
+//               {item.name}
+//             </Text>
 
-function PersonalRecipeList() {
-  const navigation = useNavigation()
-  return (
-    <View style={{ flex: 1 }}>
-      <TouchableOpacity
-        style={[
-          {
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            borderRadius: 15,
-            paddingHorizontal: 20,
-            marginTop: 5,
-            paddingVertical: 5,
-            backgroundColor: 'white',
-          },
-          styles.boxShadow,
-        ]}
-        onPress={() => {
-          navigation.navigate('PersonalList', { id: 1, name: 'My recipes' })
-        }}
-      >
-        <Text style={{ ...FONTS.h3 }}>My recipes</Text>
+//             <Image
+//               source={img}
+//               style={{
+//                 width: 50,
+//                 height: 50,
+//                 borderRadius: 45,
+//                 opacity: 0.5,
+//               }}
+//             />
+//           </TouchableOpacity>
+//         );
+//       })}
+//     </View>
+//   );
+// }
 
-        <Image
-          source={img}
-          style={{
-            width: 50,
-            height: 50,
-            borderRadius: 45,
-            opacity: 0.5,
-          }}
-        />
-      </TouchableOpacity>
-    </View>
-  )
-}
+// function PersonalRecipeList() {
+//   const navigation = useNavigation();
+//   return (
+//     <View style={{ flex: 1 }}>
+//       <TouchableOpacity
+//         style={[
+//           {
+//             flex: 1,
+//             flexDirection: "row",
+//             justifyContent: "space-between",
+//             alignItems: "center",
+//             borderRadius: 15,
+//             paddingHorizontal: 20,
+//             marginTop: 5,
+//             paddingVertical: 5,
+//             backgroundColor: "white",
+//           },
+//           styles.boxShadow,
+//         ]}
+//         onPress={() => {
+//           navigation.navigate("PersonalList", { id: 1, name: "My recipes" });
+//         }}
+//       >
+//         <Text style={{ ...FONTS.h3 }}>My recipes</Text>
+
+//         <Image
+//           source={img}
+//           style={{
+//             width: 50,
+//             height: 50,
+//             borderRadius: 45,
+//             opacity: 0.5,
+//           }}
+//         />
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
 
 // function Profile({ navigation }) {
 //   const [isCreateVisible, setCreateVisible] = useState(false)
@@ -315,105 +322,190 @@ function PersonalRecipeList() {
 //   )
 // }
 
-import Collection from '../components/Profile/Collection'
-import SortPopUp from '../components/Profile/SortPopUp'
+import Collection from "../components/Profile/Collection";
+import SortPopUp from "../components/Profile/SortPopUp";
 
-function Profile({navigation}){
+function Profile({ navigation }) {
+  const userInfo = useSelector(selectUserInfo);
+  const dispatch = useDispatch();
   const [modalVisible, setModalVisible] = useState(false);
+  const isFocused = useIsFocused();
+  const [collections, setCollections] = useState([]);
 
-  const handleCloseModal = () =>{
-    setModalVisible(false)
-  }
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
 
-  const data=[
+  useEffect(() => {
+    if (isFocused) {
+      const fetchCollections = async () => {
+        const token = await AsyncStorageService.getAccessToken();
+        try {
+          const response = await fetch(
+            `${HOST}/collections/user/${userInfo?.id}`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          const responseJson = await response.json();
+
+          if (responseJson.error) {
+            console.log(responseJson.message);
+          } else {
+            //dispatch(setUserInfo({ ...userInfo, isLogin: true }));
+            // navigation.navigate('Home')
+            setCollections(responseJson.collections);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchCollections();
+    }
+  }, [isFocused]);
+
+  const data = [
     {
-      nameCollection: 'All Yums',
-      num: 0,
-      imgCol: require('../assets/images/Profile/avatarDefault.png')
-    },
-    {
-      nameCollection: 'All Personal Recipes',
+      nameCollection: "All Personal Recipes",
       num: 5,
-      imgCol: require('../assets/images/Profile/avatarDefault.png')
+      imgCol: require("../assets/images/Profile/avatarDefault.png"),
     },
     {
-      nameCollection: 'Breakfasts',
+      nameCollection: "Breakfasts",
       num: 2,
-      imgCol: require('../assets/images/Profile/avatarDefault.png')
+      imgCol: require("../assets/images/Profile/avatarDefault.png"),
     },
     {
-      nameCollection: 'Desserts',
+      nameCollection: "Desserts",
       num: 3,
-      imgCol: require('../assets/images/Profile/avatarDefault.png')
+      imgCol: require("../assets/images/Profile/avatarDefault.png"),
     },
-    {
-      nameCollection: 'Desserts',
-      num: 3,
-      imgCol: require('../assets/images/Profile/avatarDefault.png')
-    },
-  ]
+  ];
 
-  const listCollection = data.map((col, index)=>{
+  const updateProfileImage = async (newImgUrl) => {
+    const token = await AsyncStorageService.getAccessToken();
+    try {
+      const response = await fetch(`${HOST}/users/${userInfo?.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          imgUrl: newImgUrl,
+        }),
+      });
+
+      const responseJson = await response.json();
+
+      if (responseJson.error) {
+        console.log(responseJson.message);
+      } else {
+        dispatch(setUserInfo({ ...userInfo, imgUrl: newImgUrl }));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const listCollection = collections.map((col, index) => {
     return (
-      <Collection props={col} navigation={navigation} key={index.toString()}/>
-    )
-  })
+      <Collection props={col} navigation={navigation} key={index.toString()} />
+    );
+  });
+
+  const importImage = async () => {
+    try {
+      let res = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (!res.canceled) {
+        const { fileName, uri } = res.assets[0];
+        const uploadRes = await uploadToFirebase(uri, fileName);
+        updateProfileImage(uploadRes.downloadURL);
+        // replace user state url
+      }
+    } catch (e) {
+      Alert.alert("Error uploading image");
+    }
+  };
 
   return (
-    <SafeAreaView style={{backgroundColor: 'white'}}>
+    <SafeAreaView style={{ backgroundColor: "white" }}>
       <ScrollView style={styles.container}>
         <TouchableOpacity
           onPress={() => {
-            navigation.navigate('Settings')
+            navigation.navigate("Settings");
           }}
-          style={{alignItems: 'flex-end'}}>
+          style={{ alignItems: "flex-end" }}
+        >
           <View style={styles.btnSetting}>
-            <MaterialIcons name='settings' size={24} color='black' />
+            <MaterialIcons name="settings" size={24} color="black" />
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.avatar}>
-          <Image 
+        <TouchableOpacity style={styles.avatar} onPress={importImage}>
+          <Image
             style={styles.avatarImage}
-            source={require('../assets/images/Profile/avatarTest.jpg')}/>
+            source={
+              userInfo?.imgUrl
+                ? require("../assets/images/Profile/avatarTest.jpg")
+                : { uri: userInfo.imgUrl }
+            }
+          />
         </TouchableOpacity>
-        
+
         <Text style={styles.userName}>Hà Đăng Nhuận</Text>
-        <Text style={styles.userDescription}>Edit your description and tell us a little about yourself</Text>
-        
+        <Text style={styles.userDescription}>
+          Edit your description and tell us a little about yourself
+        </Text>
+
         <View style={styles.collectionHeader}>
           <TouchableOpacity
-            onPress={()=> navigation.push('NewCollection')}
-            style={styles.btnAddColl}>
-            <MaterialIcons name='playlist-add' size={28} color='#3a9693' />
+            onPress={() => navigation.push("NewCollection")}
+            style={styles.btnAddColl}
+          >
+            <MaterialIcons name="playlist-add" size={28} color="#3a9693" />
             <Text style={[styles.titleCollection]}>New Collection</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            onPress={()=> setModalVisible(true)}
-            style={styles.btnSort}>
+            onPress={() => setModalVisible(true)}
+            style={styles.btnSort}
+          >
             <Text style={styles.titleFilter}>Sort</Text>
-            <MaterialIcons name='keyboard-arrow-down' style={{marginLeft: 5}} size={24} color='#2d6d64' />
+            <MaterialIcons
+              name="keyboard-arrow-down"
+              style={{ marginLeft: 5 }}
+              size={24}
+              color="#2d6d64"
+            />
           </TouchableOpacity>
         </View>
-        
+
         <View style={styles.listCollection}>
           {/* <Collection /> */}
           {listCollection}
         </View>
 
-        
-        <SortPopUp closePopUp={handleCloseModal} modalVisible={modalVisible}/>
+        <SortPopUp closePopUp={handleCloseModal} modalVisible={modalVisible} />
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'white',
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
   },
   btnSetting: {
     width: 35,
@@ -421,70 +513,68 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginRight: 20,
     marginBottom: 15,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 50,
-    backgroundColor: '#F3F3F3'
+    backgroundColor: "#F3F3F3",
   },
-  avatar:{
-    alignItems: 'center',
-    justifyContent: 'center',
+  avatar: {
+    alignItems: "center",
+    justifyContent: "center",
   },
-  avatarImage:{
+  avatarImage: {
     width: 80,
     height: 80,
     borderRadius: 200,
   },
-  userName:{
-    textAlign: 'center',
+  userName: {
+    textAlign: "center",
     marginTop: 15,
     fontSize: 24,
-    fontWeight: '700',
-    color: '#231F20'
+    fontWeight: "700",
+    color: "#231F20",
   },
   userDescription: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 15,
     fontSize: 13,
-    color: '#231F20'
+    color: "#231F20",
   },
-  collectionHeader:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  collectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 25,
     marginVertical: 30,
   },
-  btnAddColl:{
-    flexDirection: 'row',
-    alignItems: 'center',
-
+  btnAddColl: {
+    flexDirection: "row",
+    alignItems: "center",
   },
-  btnSort:{
-    backgroundColor: '#ecf5f4',
+  btnSort: {
+    backgroundColor: "#ecf5f4",
     paddingHorizontal: 20,
     paddingVertical: 8,
     borderRadius: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  titleCollection:{
+  titleCollection: {
     fontSize: 14,
-    fontWeight: '700',
-    color: '#231F20',
-    marginLeft: 5
+    fontWeight: "700",
+    color: "#231F20",
+    marginLeft: 5,
   },
-  titleFilter:{
+  titleFilter: {
     fontSize: 14,
-    fontWeight: '700',
-    color: '#2d6d64',
+    fontWeight: "700",
+    color: "#2d6d64",
   },
   listCollection: {
     paddingHorizontal: 25,
-  }
-  
-})
+  },
+});
 
-export default Profile
+export default Profile;
